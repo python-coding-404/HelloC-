1c6e90d82d0b66b0a6cffa87e05d0bc70390189f
python-programing <79223364+python-coding-404@users.noreply.github.com>
2022-02-09 11:22:59 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+#include <string.h>
+#include <cmath>
+#include <cstdio>
+
+
+int main(int argc, char *argv[]) {
+  //Flags 
+  cout <<"Flags:\n";
+  cout << "-v Shows The Version Number\n";
+  cout << "--file plays \n";
+   // Main loop for command line arguments
+    for (int i = 0; i < argc; i++){
+      int lol = i + 1;
+      // I wanna update to case but The code is to BIG
+        if(strcmp(argv[i], "-v") == 0){
+          system("clear");
+          cout << "Code Name MAGAPP Version 0.0.1 pre-alpha\n";
+        } 
+        if(strcmp(argv[i], "--file") == 0) {
+
+          system("clear");
+         
+        }
+          
+          
+
+      
+          
+          
+        
+          
+          
+          
+
+          
+        }
+          
+
+          
+
+
+          
+        
+          
+
+          
+          
+          
+          
+          
+        }
+    
+
+  
+
